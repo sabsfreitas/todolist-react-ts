99b5be59c2412051a8d34ccf540f32c7589fe930
Sabrina <sabrina.rf2003@gmail.com>
2023-09-12 16:04:41 -0300

    format

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,33 +1,32 @@
-import { Todo } from '../../App';
-import './styles.css';
+import { Todo } from "../../App";
+import "./styles.css";
 
-type CardProps = { 
-    todo: Todo;
-    completeTodo: (id: number) => void;
-    deleteTodo: (id: number) => void;
-}
+type CardProps = {
+  todo: Todo;
+  completeTodo: (id: number) => void;
+  deleteTodo: (id: number) => void;
+};
 
 function Card({ todo, completeTodo, deleteTodo }: CardProps) {
+  function handleCompleteTodo() {
+    completeTodo(todo.id);
+  }
 
-    function handleCompleteTodo() {
-        completeTodo(todo.id);
-    }
+  function handleDeleteTodo() {
+    deleteTodo(todo.id);
+  }
+  return (
+    <div className={`card ${todo.completed ? "done" : ""}`}>
+      <h2>{todo.title}</h2>
 
-    function handleDeleteTodo() {
-        deleteTodo(todo.id);
-    }
-    return (
-        <div className={`card ${todo.completed ? 'done' : ''}`}>
-            <h2>{todo.title}</h2>
-
-            <div className="card-buttons">
-                <button onClick={handleCompleteTodo}>
-                    {todo.completed ? 'Retomar' : 'Completar'}
-                </button>
-                <button onClick={handleDeleteTodo}>Deletar</button>
-            </div>
-        </div>
-    )
+      <div className="card-buttons">
+        <button onClick={handleCompleteTodo}>
+          {todo.completed ? "Retomar" : "Completar"}
+        </button>
+        <button onClick={handleDeleteTodo}>Deletar</button>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
